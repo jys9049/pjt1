d33ce9b59fa11d737ae37d1d242ebe1a5b20ecf0
jsy9049 <kouawb102@naver.com>
2022-07-10 12:16:01 +0900

    20220710

--- html_css/main.js
+++ html_css/main.js
@@ -2,3 +2,22 @@ const get = (target) => {
   return document.querySelector(target);
 }
 
+const getAll = (target) => {
+  return document.querySelectorAll(target);
+}
+
+const $category = get(".category-group");
+const $selectCategory = get(".select-category");
+const $selectCategoryArr = getAll(".select-category");
+const a = get("a");
+
+const categoryClick = () => {
+  $selectCategoryArr.forEach((category)=> {
+    category.addEventListener("click", (e) => {
+      const target = e.target;
+      target.classList.add('strong');
+    });
+  });
+}
+
+categoryClick();
